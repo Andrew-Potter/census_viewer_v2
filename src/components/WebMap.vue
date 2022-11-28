9c29e34d09c67989bf5153d6ecebb63f1f6a39c8
Andrew-Potter <awpotter50@gmail.com>
2022-11-28 13:18:58 -0500

    update

--- src/components/WebMap.vue
+++ src/components/WebMap.vue
@@ -17,7 +17,6 @@ import MapImageLayer from "@arcgis/core/layers/MapImageLayer"
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer"
 import ClassBreaksRenderer from "@arcgis/core/renderers/ClassBreaksRenderer"
 import Legend from "@arcgis/core/widgets/Legend"
-import LayerList from "@arcgis/core/widgets/LayerList"
 
 var baseurl = "https://ncdotsandbox.cnr.ncsu.edu/arcgis/rest/services/DemographicApp/DemographicWebMap/MapServer/"
 
@@ -100,6 +99,9 @@ export default {
 
         },
         async getAttributes(geo_index, field_alias, normalizationField){
+          console.log(geo_index)
+          console.log(field_alias)
+          console.log(normalizationField)
 
           
 
@@ -197,218 +199,218 @@ export default {
         this.map.add(this.activeGeometry)
        },
 
-       async renderLayer(layer, table_name, field_name, normalize, normalize_field, geo_name, fieldAlias, year){
-           console.log("render ")
-            console.log(layer)
-            console.log(table_name)
-            console.log(field_name)
-            console.log(normalize)
-            console.log(normalize_field),
-            console.log(geo_name)
-            console.log(fieldAlias)
-            // var new_field_name = `NCDOT_Demographics.DEMOGRAPHICAPP.%${geo_name}.${field_name}`
-            var new_field_name = `${table_name}.${field_name}`
-            console.log(new_field_name)
-          var census_data_layer = layer.sublayers.find(function(sublayer) {
-            return sublayer.title === "Census Data";
-          });
-          console.log(census_data_layer)
+      //  async renderLayer(layer, table_name, field_name, normalize, normalize_field, geo_name, fieldAlias, year){
+      //      console.log("render ")
+      //       console.log(layer)
+      //       console.log(table_name)
+      //       console.log(field_name)
+      //       console.log(normalize)
+      //       console.log(normalize_field),
+      //       console.log(geo_name)
+      //       console.log(fieldAlias)
+      //       // var new_field_name = `NCDOT_Demographics.DEMOGRAPHICAPP.%${geo_name}.${field_name}`
+      //       var new_field_name = `${table_name}.${field_name}`
+      //       console.log(new_field_name)
+      //     var census_data_layer = layer.sublayers.find(function(sublayer) {
+      //       return sublayer.title === "Census Data";
+      //     });
+      //     console.log(census_data_layer)
 
 
-          var fieldMax = {
-            onStatisticField: new_field_name, 
-            outStatisticFieldName: "fieldmax",
-            statisticType: "max"
-          };
+      //     var fieldMax = {
+      //       onStatisticField: new_field_name, 
+      //       outStatisticFieldName: "fieldmax",
+      //       statisticType: "max"
+      //     };
 
-          var fieldMin = {
-            onStatisticField: new_field_name,  
-            outStatisticFieldName: "fieldmin",
-            statisticType: "min"
-          };
+      //     var fieldMin = {
+      //       onStatisticField: new_field_name,  
+      //       outStatisticFieldName: "fieldmin",
+      //       statisticType: "min"
+      //     };
           
 
-          var max
-          var min
+      //     var max
+      //     var min
 
-          // var query = census_data_layer.createQuery();
-          // query.outStatistics = [fieldMax, fieldMin]
-          // var resp =  await census_data_layer.queryFeatures(query)
+      //     // var query = census_data_layer.createQuery();
+      //     // query.outStatistics = [fieldMax, fieldMin]
+      //     // var resp =  await census_data_layer.queryFeatures(query)
 
-          // console.log(resp)
+      //     // console.log(resp)
 
              
-          const layerColors = ["#910000", "#c33910", "#f6711f", "#fbaf52", "#ffed85"];
-          console.log(year)
-          // var res = await census_data_layer.queryFeatures({where:`${table_name}.year = DATE'${year}-1-1' `,outFields:[new_field_name]})
-          var res = await census_data_layer.queryFeatures({where:"1=1",outFields:[new_field_name]})
+      //     const layerColors = ["#910000", "#c33910", "#f6711f", "#fbaf52", "#ffed85"];
+      //     console.log(year)
+      //     // var res = await census_data_layer.queryFeatures({where:`${table_name}.year = DATE'${year}-1-1' `,outFields:[new_field_name]})
+      //     var res = await census_data_layer.queryFeatures({where:"1=1",outFields:[new_field_name]})
 
-          console.log(res)
-          var field_array = res.features.map(x=>x.attributes[new_field_name])
+      //     console.log(res)
+      //     var field_array = res.features.map(x=>x.attributes[new_field_name])
 
-          max = Math.max(...field_array)
-          min = Math.min(...field_array)
-          console.log(max)
-          console.log(min)
+      //     max = Math.max(...field_array)
+      //     min = Math.min(...field_array)
+      //     console.log(max)
+      //     console.log(min)
           
-          console.log(field_array)
+      //     console.log(field_array)
 
-          if (normalize === true){
-                var renderer = new ClassBreaksRenderer({
-                type: "class-breaks",
-                // attribute of interest - Earthquake magnitude
-                field: `${table_name}.${field_name}`,
-                normalizationField: `${table_name}.${normalize_field}`,
-                classBreakInfos: [
-                  {
-                    minValue: 0,  // 0 acres
-                    maxValue:0,  // 200,000 acres
-                    symbol: { type: "simple-fill", color: "#a2a4a6"},
-                    label: "0"  
-                  },
-                  {
-                    minValue: .01,  // 0 acres
-                    maxValue: .25,  // 200,000 acres
-                    symbol: { type: "simple-fill", color: layerColors[3]},  // will be assigned sym1
-                    label: `1 - 25 %`
-                  },
-                  {
-                    minValue: .26,  // 0 acres
-                    maxValue: .50,  // 200,000 acres
-                    symbol: { type: "simple-fill", color: layerColors[2]},  // will be assigned sym1
-                    label: `26 - 50%`
-                  },
-                  {
-                    minValue: .51,  // 0 acres
-                    maxValue: .75,  // 200,000 acres
-                    symbol: { type: "simple-fill", color: layerColors[1]},  // will be assigned sym1
-                    label: `51 - 75%`
-                  },
-                  {
-                    minValue: .76,  // 0 acres
-                    maxValue: 1,  // 200,000 acres
-                    symbol: { type: "simple-fill", color: layerColors[0]},  // will be assigned sym1
-                    label: `76 - 100%`
-                  },
+      //     if (normalize === true){
+      //           var renderer = new ClassBreaksRenderer({
+      //           type: "class-breaks",
+      //           // attribute of interest - Earthquake magnitude
+      //           field: `${table_name}.${field_name}`,
+      //           normalizationField: `${table_name}.${normalize_field}`,
+      //           classBreakInfos: [
+      //             {
+      //               minValue: 0,  // 0 acres
+      //               maxValue:0,  // 200,000 acres
+      //               symbol: { type: "simple-fill", color: "#a2a4a6"},
+      //               label: "0"  
+      //             },
+      //             {
+      //               minValue: .01,  // 0 acres
+      //               maxValue: .25,  // 200,000 acres
+      //               symbol: { type: "simple-fill", color: layerColors[3]},  // will be assigned sym1
+      //               label: `1 - 25 %`
+      //             },
+      //             {
+      //               minValue: .26,  // 0 acres
+      //               maxValue: .50,  // 200,000 acres
+      //               symbol: { type: "simple-fill", color: layerColors[2]},  // will be assigned sym1
+      //               label: `26 - 50%`
+      //             },
+      //             {
+      //               minValue: .51,  // 0 acres
+      //               maxValue: .75,  // 200,000 acres
+      //               symbol: { type: "simple-fill", color: layerColors[1]},  // will be assigned sym1
+      //               label: `51 - 75%`
+      //             },
+      //             {
+      //               minValue: .76,  // 0 acres
+      //               maxValue: 1,  // 200,000 acres
+      //               symbol: { type: "simple-fill", color: layerColors[0]},  // will be assigned sym1
+      //               label: `76 - 100%`
+      //             },
                   
-                ],
+      //           ],
 
-              });
+      //         });
 
-              }else{
-                var renderer = new ClassBreaksRenderer({
-                  type: "class-breaks",
-                  // attribute of interest - Earthquake magnitude
-                  field: `${table_name}.${field_name}`,
-                  classBreakInfos: [
-                    {
-                      minValue: 0,  // 0 acres
-                      maxValue:0,  // 200,000 acres
-                      symbol: { type: "simple-fill", color: "#a2a4a6"},
-                      label: "0"  
-                    },
-                    {
-                      minValue: min + 1,  // 0 acres
-                      maxValue: max/4,  // 200,000 acres
-                      symbol: { type: "simple-fill", color: layerColors[3]},  // will be assigned sym1
-                      label: `${min + 1} - ${max/4}`
-                    },
-                    {
-                      minValue: max/4 + 1,  // 0 acres
-                      maxValue: max/4*2,  // 200,000 acres
-                      symbol: { type: "simple-fill", color: layerColors[2]},  // will be assigned sym1
-                      label: `${max/4 + 1} - ${max/4*2}`
-                    },
-                    {
-                      minValue: max/4 *2+ 1,  // 0 acres
-                      maxValue: max/4*3,  // 200,000 acres
-                      symbol: { type: "simple-fill", color: layerColors[1]},  // will be assigned sym1
-                      label: `${max/4*2 + 1} - ${max/4*3}`
-                    },
-                    {
-                      minValue: max/4 *3+ 1,  // 0 acres
-                      maxValue: max,  // 200,000 acres
-                      symbol: { type: "simple-fill", color: layerColors[0]},  // will be assigned sym1
-                      label: `${max/4*3 + 1} - ${max}`
-                    },
+      //         }else{
+      //           var renderer = new ClassBreaksRenderer({
+      //             type: "class-breaks",
+      //             // attribute of interest - Earthquake magnitude
+      //             field: `${table_name}.${field_name}`,
+      //             classBreakInfos: [
+      //               {
+      //                 minValue: 0,  // 0 acres
+      //                 maxValue:0,  // 200,000 acres
+      //                 symbol: { type: "simple-fill", color: "#a2a4a6"},
+      //                 label: "0"  
+      //               },
+      //               {
+      //                 minValue: min + 1,  // 0 acres
+      //                 maxValue: max/4,  // 200,000 acres
+      //                 symbol: { type: "simple-fill", color: layerColors[3]},  // will be assigned sym1
+      //                 label: `${min + 1} - ${max/4}`
+      //               },
+      //               {
+      //                 minValue: max/4 + 1,  // 0 acres
+      //                 maxValue: max/4*2,  // 200,000 acres
+      //                 symbol: { type: "simple-fill", color: layerColors[2]},  // will be assigned sym1
+      //                 label: `${max/4 + 1} - ${max/4*2}`
+      //               },
+      //               {
+      //                 minValue: max/4 *2+ 1,  // 0 acres
+      //                 maxValue: max/4*3,  // 200,000 acres
+      //                 symbol: { type: "simple-fill", color: layerColors[1]},  // will be assigned sym1
+      //                 label: `${max/4*2 + 1} - ${max/4*3}`
+      //               },
+      //               {
+      //                 minValue: max/4 *3+ 1,  // 0 acres
+      //                 maxValue: max,  // 200,000 acres
+      //                 symbol: { type: "simple-fill", color: layerColors[0]},  // will be assigned sym1
+      //                 label: `${max/4*3 + 1} - ${max}`
+      //               },
                     
-                  ],
+      //             ],
 
-                });
-              }
+      //           });
+      //         }
               
 
-          console.log(renderer)
-          census_data_layer.renderer = renderer;
-          var popupTemplate = {
-                // autocasts as new PopupTemplate()
-                title: `{${table_name}.geo_name}`,
-                content: populationChange,
-                outFields: ["*"],
+      //     console.log(renderer)
+      //     census_data_layer.renderer = renderer;
+      //     var popupTemplate = {
+      //           // autocasts as new PopupTemplate()
+      //           title: `{${table_name}.geo_name}`,
+      //           content: populationChange,
+      //           outFields: ["*"],
                   
-                fieldInfos: [
-                  {
-                    fieldName: `${table_name}.${field_name}`,
-                    label: fieldAlias,
-                    visible: true,
-                    format: {
-                      digitSeparator: true,
-                      places: 0
-                    }
-                  },
-                  {
-                    fieldName: `${table_name}.${normalize_field}`,
-                    label: "Total Population",
-                    visible: true,
-                    format: {
-                      digitSeparator: true,
-                      places: 0
-                    }
-                  },
-                  {
-                    fieldName: `total_population`,
-                    label: "Total Population",
-                    visible: true,
-                    format: {
-                      digitSeparator: true,
-                      places: 0
-                    }
-                  },
-                ]
+      //           fieldInfos: [
+      //             {
+      //               fieldName: `${table_name}.${field_name}`,
+      //               label: fieldAlias,
+      //               visible: true,
+      //               format: {
+      //                 digitSeparator: true,
+      //                 places: 0
+      //               }
+      //             },
+      //             {
+      //               fieldName: `${table_name}.${normalize_field}`,
+      //               label: "Total Population",
+      //               visible: true,
+      //               format: {
+      //                 digitSeparator: true,
+      //                 places: 0
+      //               }
+      //             },
+      //             {
+      //               fieldName: `total_population`,
+      //               label: "Total Population",
+      //               visible: true,
+      //               format: {
+      //                 digitSeparator: true,
+      //                 places: 0
+      //               }
+      //             },
+      //           ]
               
                 
-              }
-            census_data_layer.popupTemplate = popupTemplate
+      //         }
+      //       census_data_layer.popupTemplate = popupTemplate
 
-            function populationChange (feature) {
-              var div = document.createElement("div");
-              // calculate the population percent change from 2010 to 2013.
-              selectedGeoId = feature.graphic.attributes[`${table_name}.geo_id`]
-              queryGeoLayers(selectedGeoId);
-              selectFeatures(feature.graphic.geometry.centroid, chart, false)
-              let proportion = feature.graphic.attributes[`${table_name}.${field_name}`] / feature.graphic.attributes[`${table_name}.${normalize_field}`] 
-              let percent = proportion * 100
-              console.log(percent)
-              div.innerHTML =
-                `${fieldAlias}: ${feature.graphic.attributes[`${table_name}.${field_name}`]} <br>
-                Total Population: ${feature.graphic.attributes[`${table_name}.${normalize_field}`]} <br>
-                Percent: ${percent.toFixed(2)}<br>
-                Year: ${new Date(feature.graphic.attributes[`${table_name}.year`]).getFullYear() + 1}
-                `
-              console.log(div.innerHTML);
-              view.popup.highlightEnabled = true
-              return div;
+      //       function populationChange (feature) {
+      //         var div = document.createElement("div");
+      //         // calculate the population percent change from 2010 to 2013.
+      //         selectedGeoId = feature.graphic.attributes[`${table_name}.geo_id`]
+      //         queryGeoLayers(selectedGeoId);
+      //         selectFeatures(feature.graphic.geometry.centroid, chart, false)
+      //         let proportion = feature.graphic.attributes[`${table_name}.${field_name}`] / feature.graphic.attributes[`${table_name}.${normalize_field}`] 
+      //         let percent = proportion * 100
+      //         console.log(percent)
+      //         div.innerHTML =
+      //           `${fieldAlias}: ${feature.graphic.attributes[`${table_name}.${field_name}`]} <br>
+      //           Total Population: ${feature.graphic.attributes[`${table_name}.${normalize_field}`]} <br>
+      //           Percent: ${percent.toFixed(2)}<br>
+      //           Year: ${new Date(feature.graphic.attributes[`${table_name}.year`]).getFullYear() + 1}
+      //           `
+      //         console.log(div.innerHTML);
+      //         view.popup.highlightEnabled = true
+      //         return div;
               
-            };
+      //       };
 
-            return layer
+      //       return layer
 
 
                     
-          // })
+      //     // })
 
 
-        }
+        // }
     },
     data(){
         return{
